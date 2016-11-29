8e3c88eae7052b8937264a10c0d851e1f4a15b08
elena_bogomolova <mavakosja@gmail.com>
2016-11-29 16:32:06 +0300

    distance calc - 1st part

--- sandbox/src/main/java/eb/vesna/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/eb/vesna/sandbox/MyFirstProgram.java
@@ -3,7 +3,18 @@ package eb.vesna.sandbox;
 public class MyFirstProgram {
 	
 	public static void main(String[] args){
-		System.out.println("Hello, Java's World!");
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(5, 7);
+        System.out.println("расстояние между точками с координатами (" + p1.x + ", " + p1.y + ") и (" + p2.x + ", " + p2.y + ") равно " + distance(p1,p2));
 	}
-	
+	public static double distance(Point p1, Point p2){
+	    double disX = p1.x - p2.x;
+	    double disY = p1.y - p2.y;
+	    double disX2 = disX * disX;
+	    double disY2 = disY * disY;
+	    return Math.sqrt(disX2 + disY2);
+    }
+
+
+
 }
---
+++ sandbox/src/main/java/eb/vesna/sandbox/Point.java
@@ -0,0 +1,15 @@
+package eb.vesna.sandbox;
+
+/**
+ * Created by Elena_Bogomolova on 29.11.2016.
+ */
+public class Point {
+
+    public double x;
+    public double y;
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+
+    }
+}
