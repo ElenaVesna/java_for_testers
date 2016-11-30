4785ce4575a9f2c846d4369c72c4888e437ebe79
elena_bogomolova <mavakosja@gmail.com>
2016-11-30 18:31:28 +0300

    Tests for Creation Contacts

--- addressbook-web-tests/src/test/java/eb/vesna/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/eb/vesna/addressbook/ContactCreationTests.java
@@ -33,13 +33,17 @@ public class ContactCreationTests {
     @Test
     public void testContactCreation() {
         gotoAddNewContact();
-        enterFirstName();
-        enterLastName();
-        enterMobilePhone();
-        enterEmail();
-        enterAddress();
+        fillContactForm(new ContactData("Elena", "Vesna", "+79000000", "test@test.com", "Ryazan"));
         submitContactCreation();
         returnToHomePage();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        enterFirstName(contactData.getName());
+        enterLastName(contactData.getLastName());
+        enterMobilePhone(contactData.getMobilePhone());
+        enterEmail(contactData.getEmail());
+        enterAddress(contactData.getAddress());
     }
 
     private void returnToHomePage() {
@@ -50,34 +54,34 @@ public class ContactCreationTests {
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void enterAddress() {
+    private void enterAddress(String address) {
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("Ryazan");
+        wd.findElement(By.name("address")).sendKeys(address);
     }
 
-    private void enterEmail() {
+    private void enterEmail(String email) {
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("test@test.com");
+        wd.findElement(By.name("email")).sendKeys(email);
     }
 
-    private void enterMobilePhone() {
+    private void enterMobilePhone(String mobilePhone) {
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("+79000000");
+        wd.findElement(By.name("mobile")).sendKeys(mobilePhone);
     }
 
-    private void enterLastName() {
+    private void enterLastName(String lastName) {
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Vesna");
+        wd.findElement(By.name("lastname")).sendKeys(lastName);
     }
 
-    private void enterFirstName() {
+    private void enterFirstName(String firstName) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Elena");
+        wd.findElement(By.name("firstname")).sendKeys(firstName);
     }
 
     private void gotoAddNewContact() {
---
+++ addressbook-web-tests/src/test/java/eb/vesna/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package eb.vesna.addressbook;
+
+public class ContactData {
+    private final String name;
+    private final String lastName;
+    private final String mobilePhone;
+    private final String email;
+    private final String address;
+
+    public ContactData(String name, String lastName, String mobilePhone, String email, String address) {
+        this.name = name;
+        this.lastName = lastName;
+        this.mobilePhone = mobilePhone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
