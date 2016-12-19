c78b69e2345062061f3818ac4ede53aa7c6685bd
elena_bogomolova <mavakosja@gmail.com>
2016-12-19 16:30:20 +0300

    лямбда-выражения при создании групп и контактов

--- addressbook-web-tests/src/test/java/eb/vesna/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/eb/vesna/addressbook/tests/ContactCreationTests.java
@@ -1,7 +1,6 @@
 package eb.vesna.addressbook.tests;
 
 import eb.vesna.addressbook.models.ContactData;
-import eb.vesna.addressbook.models.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,18 +13,14 @@ public class ContactCreationTests extends TestBase {
     public void testContactCreation() {
         app.getNavigationHelper().gotoHomePage();
         List<ContactData> before = app.getContactHelper().getContactList();
-        ContactData contact = new ContactData("Elena", "Vesna", "+79000000", "test@test.com", "Ryazan", "testGroup2");
+        ContactData contact = new ContactData("Elena-lambda", "Vesna-l", "+79000000", "test@test.com", "Ryazan", "testGroup2");
         app.getContactHelper().createContact(contact);
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (ContactData c : after) {
-            if (c.getId() > max) {
-                max = c.getId();
-            }
-        }
-        contact.setId(max);
+
+        //Comparator<? super ContactData> byID = (Comparator<ContactData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
