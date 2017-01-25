cdef3edde45ea85cb0e6443cdb5d771e65f50b5e
elena_bogomolova <mavakosja@gmail.com>
2017-01-25 16:31:58 +0300

    добавлены тесты Login, Registration + ленивая инициализация

--- mantis-tests-new/src/test/java/eb/vesna/mantis/tests/TestBase.java
+++ mantis-tests-new/src/test/java/eb/vesna/mantis/tests/TestBase.java
@@ -1,6 +1,6 @@
 package eb.vesna.mantis.tests;
 
-import eb.vesna.addressbook.appmanager.ApplicationManager;
+import eb.vesna.mantis.appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeSuite;
 public class TestBase {
 
     public static ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite
     public void setUp() throws Exception {
